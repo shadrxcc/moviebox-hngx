a4d697df42bdc8c4e059b472f3f2c29db1dd1134
shadrxcc <akaadesm@gmail.com>
2023-09-11 19:09:12 +0100

    fix header responsiveness

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,13 +4,13 @@ import menu from "../assets/menu.svg";
 
 const Header = () => {
   return (
-    <header className="text-white py-6 px-8 lg:px-20 flex justify-between items-center">
-      <div className="flex max-w-full gap-x-6 items-center">
+    <header className="text-white py-6 px-4 lg:px-20 flex justify-between items-center">
+      <div className="flex max-w-full gap-x-2 md:gap-x-6 items-center">
         <img className="w-[40px]" src={logo} alt="moviebox logo" />
         <p className="text-lg font-bold leading-6">MovieBox</p>
       </div>
 
-      <form action="search">
+      <form className="hidden md:block" action="search">
         <div className="flex px-[10px] md:w-[25rem] lg:w-[35rem] py-[6px] border rounded-md border-[#D1D5DB] items-center">
           <input
             className="outline-none placeholder:text-white bg-transparent border-none w-full"
@@ -23,8 +23,9 @@ const Header = () => {
         </div>
       </form>
 
-      <div className="flex gap-x-7 items-center">
-        <p className="text-base font-bold leading-6">Sign in</p>
+      <div className="flex gap-x-4 md:gap-x-7 items-center">
+        <img className="md:hidden" src={search} alt="" />
+        <p className="text-base hidden md:block font-bold leading-6">Sign in</p>
         <img src={menu} alt="" />
       </div>
     </header>
