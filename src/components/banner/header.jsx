6150608d474cf2cdd4bb5019b48147d98e1a0248
shadrxcc <akaadesm@gmail.com>
2023-09-12 09:59:24 +0100

    fix menu import on header component

--- src/components/banner/header.jsx
+++ src/components/banner/header.jsx
@@ -1,6 +1,6 @@
 import logo from "../../assets/tv.svg";
 import search from "../../assets/Search.svg";
-import menu from "../../assets/menu.svg";
+import menu from "../../assets/Menu.svg";
 import { useLocation } from "react-router-dom";
 
 const Header = () => {
